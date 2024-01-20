52cfd22911ab8552d7cdbd6463a245fefa256895
elkomy2001 <abdallahshaban200@gmail.com>
2024-01-20 22:57:13 +0200

    finish splash animation

--- lib/Features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widget/splash_view_body.dart
@@ -9,7 +9,29 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> {
+class _SplashViewBodyState extends State<SplashViewBody>
+ with SingleTickerProviderStateMixin {
+ late AnimationController animationController;
+ late Animation<Offset> slidingAnimaton;
+
+
+ @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this ,
+    duration: const Duration(seconds: 1),
+    );
+    slidingAnimaton = Tween<Offset>(begin: const Offset(0, 2),
+    end: Offset.zero).animate(animationController);
+
+    animationController.forward();
+    slidingAnimaton.addListener(() {});
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +40,17 @@ class _SplashViewBodyState extends State<SplashViewBody> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetData.logo),
-       const Text('Read Free Books ..' , 
-       style: FontStyles.fontSize16, textAlign: TextAlign.center,
-       ),
+        AnimatedBuilder(
+          animation: slidingAnimaton,
+          builder: (context, child) {
+            return SlideTransition(
+          position: slidingAnimaton,
+           child: const Text('Read Free Books ..' , 
+           style: FontStyles.fontSize16, textAlign: TextAlign.center,
+           ),
+         );
+          },
+       )
       ],
     ));
   }
